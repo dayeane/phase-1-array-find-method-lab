1d32d016221ce61e174bc6781ecb7c5a97c3cf69
Dayeane <dayeane85@gmail.com>
2021-10-13 20:23:17 -0700

    commit tree 088c9306865b17b88eea73f9ee0196798c3a4d91

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-
+const record = [
+        {year: "2018", result: "L"},
+        {year: "2016", result: "N/A"}
+        //...
+      ]
+    
+    function superbowlWin(arr) {
+        const response = arr.find(element => element.result === "W");
+        if (response === undefined) {
+            return response
+        } else {
+            return response.year
+        }
+    }
+    
+    
+    
+    
