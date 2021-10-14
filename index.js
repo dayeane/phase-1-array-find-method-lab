30b0afea9518fee166c8f2384808bb42f1e12305
Dayeane <dayeane85@gmail.com>
2021-10-13 20:24:39 -0700

    array find method lab

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 
+    
+    function superbowlWin(arr) {
+        const response = arr.find(element => element.result === "W");
+        if (response === undefined) {
+            return response
+        } else {
+            return response.year
+        }
+    }
+    
+    
+    
+    
